711b385b346a36d66393d8d7c2d083f99612c721
lenbo <allenbora@gmail.com>
2022-12-26 18:04:47 +0800

    Twenty-Oneth

---
+++ FortuneProg.py
@@ -0,0 +1,19 @@
+# Fortune Cookie Program 🥠
+#The user may change the fortune answers statement...
+#Fortune program based on random numbers.....
+import random
+
+fortune = random.randint(0, 4)
+
+if fortune == 0:
+  print("May you one day be carbon neutral")
+elif fortune == 1:  
+  print("You have rice in your teeth")
+elif fortune == 2:
+  print("No snowflake feels responsible for an avalanche")
+elif fortune == 3:
+  print("You can only connect the dots looking backwards")
+elif fortune == 4:
+  print("The fortune you seek is in another cookie")
+
+  # End..........
